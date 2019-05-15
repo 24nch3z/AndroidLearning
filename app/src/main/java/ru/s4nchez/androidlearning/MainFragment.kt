7b549f707a1017229f29e500eb1f5ac549962dbb
24nch3z <sanchezzzgg22@gmail.com>
2019-05-15 10:51:33 +0300

    Headless Fragment 6 (добавил child фрагмент)

--- app/src/main/java/ru/s4nchez/androidlearning/MainFragment.kt
+++ app/src/main/java/ru/s4nchez/androidlearning/MainFragment.kt
@@ -5,15 +5,8 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.activity_main.*
-import ru.s4nchez.androidlearning.headless.HeadlessFragment
-import ru.s4nchez.androidlearning.headless.HeadlessFragmentContract
-import ru.s4nchez.androidlearning.headless.HeadlessFragmentListener
 
-class MainFragment : Fragment(), HeadlessFragmentListener {
-
-    private val HEADLESS_TAG = "headless"
-    private var headlessFragment: HeadlessFragment? = null
+class MainFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
@@ -21,28 +14,10 @@ class MainFragment : Fragment(), HeadlessFragmentListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        headlessFragment = fragmentManager!!.findFragmentByTag(HEADLESS_TAG) as HeadlessFragment?
-        if (headlessFragment == null) {
-            headlessFragment = HeadlessFragment()
-            fragmentManager!!.beginTransaction()
-                    .add(headlessFragment!!, HEADLESS_TAG)
+        if (savedInstanceState == null) {
+            childFragmentManager.beginTransaction()
+                    .replace(R.id.container_fragment, ChildFragment())
                     .commit()
         }
-
-        (headlessFragment as HeadlessFragmentContract?)?.let {
-            it.setListener(this)
-            it.load()
-        }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        headlessFragment = null
-    }
-
-    override fun onLoad(result: Double) {
-        Logger.l("Call in SecondActivity")
-        action.text = result.toString()
     }
 }
