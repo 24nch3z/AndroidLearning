439cc4fc7d82973390120e9d5a47b2f4fef57e58
24nch3z <sanchezzzgg22@gmail.com>
2019-07-11 11:35:19 +0300

    Красил иконки

--- app/src/main/java/ru/s4nchez/androidlearning/MainActivity.kt
+++ app/src/main/java/ru/s4nchez/androidlearning/MainActivity.kt
@@ -1,12 +1,29 @@
 package ru.s4nchez.androidlearning
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
+import android.graphics.PorterDuff
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val color = Color.BLACK
+        val mode = PorterDuff.Mode.XOR
+
+        setColor(image_left_top, color, mode)
+        setColor(image_right_top, color, mode)
+        setColor(image_left_bottom, color, mode)
+        setColor(image_right_bottom, color, mode)
+    }
+
+    private fun setColor(imageView: ImageView, color: Int, mode: PorterDuff.Mode) {
+        val imageLeftTopDrawable = imageView.drawable
+        imageLeftTopDrawable.setColorFilter(color, mode)
     }
 }
